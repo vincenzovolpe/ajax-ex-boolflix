3168c547e31617b1086e67a37a1431a17c1251ac
vincenzovolpe <vinci792010@gmail.com>
2019-12-16 19:08:51 +0100

    Aggiunta prima versione creazione voto film con le stelline

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Memorizzo in una variabile  il percorso base  delle API
+// Memorizzo in una variabile il percorso base  delle API
 var api_url_base = 'https://api.themoviedb.org/3/';
 
 $(document).ready(function(){
@@ -48,8 +48,10 @@ function cercaFilm() {
                     }
                     // Creo il template
                     var html = template_function(variabili);
-                    // Lo appendo al contenitore dei dischi
+                    // Lo appendo al contenitore dei film
                     $('.contenitore-film').append(html);
+                    var punti = creaPuntiStelle(film[i].vote_average);
+                    $('.contenitore-film').children(':last').append(punti);
                 }
             },
             error: function() {
@@ -59,4 +61,43 @@ function cercaFilm() {
     }
     // Pulisco la barra di ricerca
     $('.cerca_film').val('');
+}
+
+function creaPuntiStelle(voto) {
+    var num_arrotondato = Math.round(voto);
+    console.log('Voto arrotondato: ' + num_arrotondato);
+    var num_convertito = convertiPunti(num_arrotondato);
+    console.log('Voto convertito: ' + num_convertito);
+    var stelle = '';
+    /*if (num_convertito == 0) {
+
+    } else {
+
+    }*/
+    for (var j = 0; j < num_convertito; j++) {
+            stelle = stelle + '<i class="fas fa-star yellow"></i>';
+    }
+    /*if (num_convertito < 5) {
+        for (var i = 0; i < (5 - num_convertito); i++) {
+            stelle = stelle + '<i class="fas fa-star grey"></i>';
+        }
+    } */
+    return stelle;
+    //$('.film>ul>li:last-child').append(stelle);
+}
+
+function convertiPunti(num_arrotondato) {
+    console.log('Voto passato a convertiPunti(): ' + num_arrotondato);
+    switch(true) {
+      case (num_arrotondato == 1) || (num_arrotondato == 2):
+        return 1;
+      case (num_arrotondato == 3) || (num_arrotondato == 4):
+        return 2;
+      case (num_arrotondato == 5) || (num_arrotondato == 6):
+        return 3;
+      case (num_arrotondato == 7) || (num_arrotondato == 8):
+         return 4;
+      default:
+         return 5;
+    }
 }
